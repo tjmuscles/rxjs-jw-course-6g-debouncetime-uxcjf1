c3581d72f6444511026ec915caf8d7e7b5daf927
tjmuscles <tjmuscles@yahoo.com>
2022-07-15 10:35:40 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
+import { fromEvent } from "rxjs";
+import { map, debounceTime } from 'rxjs/operators';
+
 const sliderInput = document.querySelector('input#slider');
 
+fromEvent(sliderInput, 'input')
+  .pipe(
+    debounceTime(1000),
+    map(event => event.target['value'])
+  )
+  .subscribe(val =>  console.log(val));
